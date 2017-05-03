b2249c0970fb95b96ad43d1ee531cd32ef6d6ab8
McThuggin <jstnmcbrd@outlook.com>
2017-05-02 19:42:58 -0700

    Test Run
    I had to add some stuff, but now the bot starts up without any errors.

--- bot.js
+++ bot.js
@@ -1,16 +1,34 @@
-console.log('The RPGT Bot is starting');
+console.log('initializing...');
+
+var Twit = require('twit');
+
+var config = require('./config');
+var T = new Twit(config);
+
+var stream = T.stream('user');
+
+stream.on('tweet', tweetEvent);
 
 start();
 
 
 function start()
 {
-	
+	console.log('The RPGT Bot has started');
 }
 
 function tweetEvent(eventMsg) {
+	
+	var replyTo = eventMsg.in_reply_to_screen_name;
+	var text = eventMsg.text;
+	var senderUserName = eventMsg.user.screen_name;
+	var senderName = eventMsg.user.name;
 
-
+	if(replyTo === 'JohnLockeBot')
+	{
+		text = text.replace(/@JohnLockeBot /g, '');
+		//var gReturn = guess(text, senderName, senderUserName);
+	}
 
 }
 
@@ -27,13 +45,17 @@ function tweet(txt) {
 	function tweeted(err, data, response) {
 		if (err) {
 			console.log("Somthing went wrong when trying to tweet! Dumping to err file.");
-			var fs = require('fs');
-			var json = JSON.stringify(err, null, 2);
-			fs.writeFile("err.json", json);
+			dumpError(err);
 		}
 		else
 		{
 
 		}
 	}
+}
+
+function dumpError(err) {
+	var fs = require('fs');
+	var json = JSON.stringify(err, null, 2);
+	fs.writeFile("err.json", json);
 }
